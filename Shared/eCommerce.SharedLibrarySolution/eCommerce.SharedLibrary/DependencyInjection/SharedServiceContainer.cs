789229a4c5c2f2b1ca332cf9e0a427cea5bec928
MrEshboboyev <mreshboboyev@gmail.com>
2025-04-02 07:31:12 -0700

    Implement JWT authentication and project structure
    This commit introduces a new .NET project structure with a focus on JWT authentication and centralized package management. Key files such as `Directory.Build.props` and `Directory.Packages.props` are added for project properties and package version management.
    
    The solution file `eCommerce.SharedLibrarySolution.sln` organizes the project, which includes the new `eCommerce.SharedLibrary` project. The `eCommerce.SharedLibrary.csproj` is configured to reference essential NuGet packages for ASP.NET Core, Entity Framework Core, and Serilog.
    
    New classes `JWTAuthenticationScheme`, `SharedServiceContainer`, and middleware classes `GlobalException` and `ListenToOnlyApiGateway` are implemented to enhance authentication, error handling, and service registration. Additionally, an interface `IGenericInterface` for CRUD operations and a standardized `Response` record are introduced to improve code reusability and response structure.

---
+++ Shared/eCommerce.SharedLibrarySolution/eCommerce.SharedLibrary/DependencyInjection/SharedServiceContainer.cs
@@ -0,0 +1,51 @@
+﻿using eCommerce.SharedLibrary.Middleware;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+
+namespace eCommerce.SharedLibrary.DependencyInjection;
+
+public static class SharedServiceContainer
+{
+    public static IServiceCollection AddSharedService<TContext>(
+        this IServiceCollection services,
+        IConfiguration config, 
+        string fileName) where TContext : DbContext
+    {
+        // Add Generic Database context
+        services.AddDbContext<TContext>(option =>
+        {
+            option.UseNpgsql(config.GetConnectionString("eCommerceConnection"));
+        });
+
+        // configure serilog logging
+        Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Information()
+            .WriteTo.Debug()
+            .WriteTo.Console()
+            .WriteTo.File(
+                path: $"{fileName}-.text",
+                restrictedToMinimumLevel: Serilog.Events.LogEventLevel.Information,
+                outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {message:lj}{NewLine}{Exception}", 
+                rollingInterval: RollingInterval.Day)
+            .CreateLogger();
+
+        // Add JWT Authentication Scheme 
+        JWTAuthenticationScheme.AddJWTAuthenticationScheme(services, config);
+
+        return services;
+    }
+
+    public static IApplicationBuilder UseSharedPolicies(this IApplicationBuilder app)
+    {
+        // Use Global Exception
+        app.UseMiddleware<GlobalException>();
+
+        // Register Middleware to block all outsiders API calls
+        app.UseMiddleware<ListenToOnlyApiGateway>();
+
+        return app;
+    }
+}
